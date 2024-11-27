929737cdb9af163b54cc89bedbcad6bfd2052e6f
GabrielDomiciano2008 <00001136577853SP@al.educacao.sp.gov.br>
2024-11-27 12:18:37 -0300

    Cartões de Perguntas do Projeto na Alura

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+   'Programação',
+   'O que é Python?',
+   'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'Paris é a capital da França. A cidade é conhecida por sua arte, moda, gastronomia e cultura. Entre seus principais pontos turísticos, estão a Torre Eiffel, o Museu do Louvre, o Arco do Triunfo e o Palácio de Versalhes, além do rio Sena. Em 2024, ela recebe os Jogos Olímpicos de Verão. (Fonte: Folha de S.Paulo - UOL)'
+)
+criaCartao(
+    'Programação',
+    'o que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é hi (RAI)'
+)
+
+criaCartao(
+    'Filosofia',
+    'A mente é igual ao cérebro ou temos almas?',
+    'Uma alma não tem mente nem cérebro. Está amarrado com a mente. Quando a alma desceu à terra para ter uma experiência física, ela levou consigo a mente. A mente foi criada para seguir as instruções da alma. Mas com o passar do tempo a mente assumiu o controle da alma e tornou-se a executora. Cinco sentidos escravizaram a mente e a mente começou a seguir os ditames dos sentidos. Os sentidos continuam puxando a mente em direções diferentes e a alma também tem que seguir a mente. (fonte: Quora)'
+ )
+ criaCartao(
+     'Filosofia',
+     'O que é consciência?',
+     'Consciência é tudo que você experimenta. É a melodia gravada na sua cabeça, a doçura da mousse de chocolate, a dor latejante de uma dor de dente, o amor feroz pelo seu filho e a amarga consciência de que eventualmente todos os sentimentos acabarão. A origem e a natureza destas experiências, por vezes referidas como qualia, têm sido um mistério desde os primeiros dias da antiguidade até ao presente. Muitos filósofos analíticos da mente modernos, mais proeminentemente talvez Daniel Dennett, da Tufts University, consideram a existência da consciência uma afronta tão intolerável ao que eles acreditam que deveria ser um universo de matéria sem sentido e ao vazio que declaram que é uma ilusão. (Fonte: Scientific American)'
+ )
+ criaCartao(
+     'Geografia',
+     'Qual é a montanha mais alta da África?',
+     'Localizado na Tanzânia, o Monte Kilimanjaro é o pico mais alto do continente africano, com 5.895 metros (19.340 pés). A majestosa montanha é um vulcão coberto de neve. (Fonte: National Geographic Society)'
+ )
+ 
+ criaCartao(
+     'Lingua Portuguesa',
+     'Qual nome de país possui um tritongo?',
+     'Uruguai'
+ )
+ criaCartao(
+    'História',
+    'Quando terminou oficialmente a Primeira Guerra Mundial?',
+    'A Primeira Guerra Mundial terminou em 11 de novembro de 1918 com a assinatura do Armistício de Compiègne. De acordo com a Associação Comemorativa do Armistício (Armistice Memorial Association), a última reunião antes de fechado o acordo começou às 2h15 da madrugada e contou com três horas de discussão. (Fonte: National Geographic Brasil)'
+)
